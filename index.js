40a749e62898de556a76f1a2b09337870fbc1503
natrdguerra <natalia.rdguerra@gmail.com>
2024-11-02 21:41:37 -0300

    Iniciando o projeto nodeJS

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('querystring');
 
-// Middleware para analisar dados do formulário
-app.use(express.urlencoded({ extended: false }));
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        fs.readFile('./index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Erro ao carregar o arquivo');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.method === 'POST' && req.url === '/submit') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const formData = parse(body);
+            console.log('Dados recebidos do formulário:', formData);
 
-// Rota para o formulário
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>Formulario recebido com sucesso!</h1>');
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Rota não encontrada');
+    }
 });
 
-// Rota para processar os dados do formulário
-app.post('/cadastro', (req, res) => {
-  const nome = req.body.nome;
-})
+server.listen(3001, () => {
+    console.log('Servidor rodando em http://localhost:3001');
+});
